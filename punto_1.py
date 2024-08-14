d85993fc3f68e5ecf48e7bb469fc66e5e01a4859
carlosmenesessBv <carlos2008meneses@gmail.com>
2024-08-14 15:01:40 -0500

    Add files via upload

---
+++ punto_1.py
@@ -0,0 +1,16 @@
+# Datos por teclado para un único empleado
+
+# Solicitar datos al usuario
+numero_identificacion = input("Ingrese el número de identificación del empleado: ")
+nombres = input("Ingrese los nombres del empleado: ")
+apellidos = input("Ingrese los apellidos del empleado: ")
+direccion = input("Ingrese la dirección del empleado: ")
+telefono = input("Ingrese el número de teléfono del empleado: ")
+edad = int(input("Ingrese la edad del empleado: "))
+genero = input("Ingrese el género del empleado: ")
+estado_civil = input("Ingrese el estado civil del empleado: ")
+numero_hijos = int(input("Ingrese el número de hijos del empleado: "))
+estatura = float(input("Ingrese la estatura en metros del empleado: "))
+fecha_contratacion = input("Ingrese la fecha de contratación del empleado (Día, mes y año): ")
+sueldo_basico = float(input("Ingrese el sueldo básico del empleado: "))
+dias_laborados = int(input("Ingrese el número de días laborados del empleado: "))
